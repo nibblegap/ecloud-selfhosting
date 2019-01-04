44873df21caa7ac5a5e17209b5d7e47dcdb47576
thilo <thilo@eelo.io>
2019-01-04 13:14:00 +0100

    - added admin user creation for NC, alternative method

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# get info from env
 ENVFILE="/mnt/docker/.env"
 NEXTCLOUD_ADMIN_USER=$(grep ^NEXTCLOUD_ADMIN_USER= "$ENVFILE" | awk -F= '{ print $NF }')
 NEXTCLOUD_ADMIN_PASSWORD=$(grep ^NEXTCLOUD_ADMIN_PASSWORD= "$ENVFILE" | awk -F= '{ print $NF }')
@@ -9,13 +10,26 @@ MYSQL_USER_NC=$(grep ^MYSQL_USER_NC= "$ENVFILE" | awk -F= '{ print $NF }')
 MYSQL_PASSWORD_NC=$(grep ^MYSQL_PASSWORD_NC= "$ENVFILE" | awk -F= '{ print $NF }')
 DRIVE_SMTP_PASSWORD=$(grep ^DRIVE_SMTP_PASSWORD= "$ENVFILE" | awk -F= '{ print $NF }')
 
-sed -i "s/localhost/'drive.$DOMAIN'/g" /mnt/docker/nextcloud/config/config.php
-sed -i "s/);//g" /mnt/docker/nextcloud/config/config.php
+PFA_SETUP_PASSWORD=$(grep ^PFA_SETUP_PASSWORD= "$ENVFILE" | awk -F= '{ print $NF }')
 
-/bin/echo -e "   'dbuser' => '$MYSQL_USER_NC',\n   'dbpassword' => '$MYSQL_PASSWORD_NC',\n   'skeletondirectory' => '',\n   'mail_from_address' => 'drive',\n   'mail_smtpmode' => 'smtp',\n   'mail_smtpauthtype' => 'PLAIN',\n   'mail_domain' => '$DOMAIN',\n   'mail_smtpauth' => 1,\n   'mail_smtphost' => 'mail.$DOMAIN',\n   'mail_smtpname' => 'drive@$DOMAIN',\n   'mail_smtppassword' => '$DRIVE_SMTP_PASSWORD',\n   'mail_smtpport' => '587',\n   'mail_smtpsecure' => 'tls',   'installed' => true,\n);" >> /mnt/docker/nextcloud/config/config.php
+# tweak nextcloud config
+sed -i "s/localhost/drive.$DOMAIN/g" /mnt/docker/nextcloud/config/config.php
+sed -i "s/);//g" /mnt/docker/nextcloud/config/config.php
+/bin/echo -e "   'dbuser' => '$MYSQL_USER_NC',\n   'dbpassword' => '$MYSQL_PASSWORD_NC',\n   'skeletondirectory' => '',\n   'mail_from_address' => 'drive',\n   'mail_smtpmode' => 'smtp',\n   'mail_smtpauthtype' => 'PLAIN',\n   'mail_domain' => '$DOMAIN',\n   'mail_smtpauth' => 1,\n   'mail_smtphost' => 'mail.$DOMAIN',\n   'mail_smtpname' => 'drive@$DOMAIN',\n   'mail_smtppassword' => '$DRIVE_SMTP_PASSWORD',\n   'mail_smtpport' => '587',\n   'mail_smtpsecure' => 'tls',\n   'installed' => true,\n);" >> /mnt/docker/nextcloud/config/config.php
 touch /mnt/docker/nextcloud/data/.ocdata
+
+# create admin account for nextcloud
 docker cp /mnt/docker/deployment/ncdb-templates/resetpw.sh nextcloud:/tmp/ && docker exec -t nextcloud bash /tmp/resetpw.sh $NEXTCLOUD_ADMIN_USER $NEXTCLOUD_ADMIN_PASSWORD
 docker restart nextcloud
+
+# create postfix DB schema
+curl --silent -L https://mail.$DOMAIN/setup.php > /dev/null
+
+# set pfa setup password
+SETUPPW_HASH=$(docker exec -t postfixadmin php /postfixadmin/pwgen.php "$PFA_SETUP_PASSWORD" | tail -n1)
+sed -i "s|\($CONF\['setup_password'\].*=\).*|\1 '${SETUPPW_HASH}';|" /mnt/docker/postfixadmin/config.inc.php
+
+# display DKIM DNS setup info/instructions to the user
 clear
 echo "Please add the following records to your domain's DNS configuration."
 find /mnt/docker/mail/dkim/ -maxdepth 1 -mindepth 1 -type d | while read line; do DOMAIN=$(basename $line); echo "  - DKIM record (TXT) for $DOMAIN:" && cat $line/public.key; done
