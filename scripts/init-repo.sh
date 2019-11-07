df403d5c1d3261b3b52d77ed9c5c6c876fec7132
Felix Ableitner <me@nutomic.com>
2019-11-07 13:28:11 +0100

    Use correct template variables for Nextcloud config

--- scripts/init-repo.sh
+++ scripts/init-repo.sh
@@ -173,6 +173,10 @@ fi
 # create nextcloud config
 mkdir -p "/mnt/repo-base/volumes/nextcloud/config/"
 cat /mnt/repo-base/templates/nextcloud/config.php | sed "s/@@@DOMAIN@@@/$DOMAIN/g" | \
+    sed "s/@@@DRIVE_SMTP_PASSWORD@@@/$DRIVE_SMTP_PASSWORD/g" | sed "s/@@@MYSQL_PASSWORD_NC@@@/$MYSQL_PASSWORD_NC/g" | \
+    sed "s/@@@MYSQL_DATABASE_NC@@@/$MYSQL_DATABASE_NC/g" | sed "s/@@@MYSQL_USER_NC@@@/$MYSQL_USER_NC/g" > \
+    "/mnt/repo-base/volumes/nextcloud/config/config.php"
+
     sed "s/@@@DRIVE_SMTP_PASSWORD@@@/$DRIVE_SMTP_PASSWORD/g" > "/mnt/repo-base/volumes/nextcloud/config/config.php"
 chown www-data:www-data "/mnt/repo-base/volumes/nextcloud/" -R
 
--- templates/nextcloud/config.php
+++ templates/nextcloud/config.php
@@ -48,9 +48,9 @@ $CONFIG = array (
     'db_type' => 'mariadb',
     'db_host' => 'mariadb',
     'db_port' => '3306',
-    'db_name' => 'postfix',
-    'db_user' => 'postfix',
-    'db_password' => 'NL8Jqyqt_PF1',
+    'db_name' => '@@@MYSQL_DATABASE_NC@@@',
+    'db_user' => '@@@MYSQL_USER_NC@@@',
+    'db_password' => '@@@MYSQL_PASSWORD_NC@@@',
     'mariadb_charset' => 'utf8mb4',
     'queries' =>
     array (
