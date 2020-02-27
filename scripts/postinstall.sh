57721dbd0da371b3ac9bf5fda5373efa40af6992
Felix <felix@e.email>
2020-02-27 10:19:36 +0000

    Revert broken commits by vincent
    This reverts commit 97b7d2cd3458cbcd3187b2532864a12e2f3343e6.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 set -ex
 
+# TODO: it looks like this script is executed twice when installing with ansible
+
 source /mnt/repo-base/scripts/base.sh
 
 echo -e "\nHack: restart everything to ensure that database and nextcloud are initialized"
 docker-compose restart
 
 printf "$(date): Waiting for Nextcloud to finish installation"
-# sleep for 300 seconds
-for i in {0..300}; do
-  sleep 1
-  printf "."
-done
+sleep 300
 
 # Create Nextcloud mysql database and user
 docker-compose exec -T mariadb mysql --user=root --password="$MYSQL_ROOT_PASSWORD" \
